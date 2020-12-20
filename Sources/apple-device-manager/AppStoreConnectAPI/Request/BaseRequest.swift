a2319b9db57ae873a8c686646f5481b3c497e78f
thedoritos <thedoritos@gmail.com>
2020-12-20 13:19:52 +0900

    :sparkles: Add devices request

---
+++ Sources/apple-device-manager/AppStoreConnectAPI/Request/BaseRequest.swift
@@ -0,0 +1,44 @@
+import Foundation
+import APIKit
+
+protocol BaseRequest: Request {
+    var token: APIToken { get }
+}
+
+extension BaseRequest {
+    var baseURL: URL { return URL(string: "https://api.appstoreconnect.apple.com/v1/")! }
+
+    func intercept(urlRequest: URLRequest) throws -> URLRequest {
+        var mutableRequest = urlRequest
+        mutableRequest.addValue("Bearer \(token.value)", forHTTPHeaderField: "Authorization")
+        return mutableRequest
+    }
+
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        guard let response = object as? Response else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        return response
+    }
+}
+
+extension BaseRequest where Response: Decodable {
+    var dataParser: DataParser { return DecodableDataParser<Response>() }
+}
+
+struct DecodableDataParser<T: Decodable>: DataParser {
+    let contentType: String? = "application/json"
+
+    func parse(data: Data) throws -> Any {
+        let decoder = JSONDecoder()
+        let dateDecoder = ISO8601DateFormatter()
+        dateDecoder.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        decoder.dateDecodingStrategy = .custom() {
+            let container = try $0.singleValueContainer()
+            let dateString = try container.decode(String.self)
+            return dateDecoder.date(from: dateString)!
+        }
+
+        return try decoder.decode(T.self, from: data)
+    }
+}
---
+++ Sources/apple-device-manager/AppStoreConnectAPI/Request/GetDevicesRequest.swift
@@ -0,0 +1,10 @@
+import APIKit
+
+struct GetDeivcesRequest: BaseRequest {
+    typealias Response = DevicesResopnse
+
+    let method: HTTPMethod = .get
+    let path: String = "devices"
+
+    let token: APIToken
+}
