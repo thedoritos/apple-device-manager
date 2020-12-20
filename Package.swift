0b8cdd9d878cf833d1bbfeabd5e1443e47d2287e
thedoritos <thedoritos@gmail.com>
2020-12-20 18:00:58 +0900

    :heavy_minus_sign: Remove APIKit
    - It fails to be built on Linux

--- Package.swift
+++ Package.swift
@@ -9,16 +9,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.1"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
-        .package(url: "https://github.com/ishkawa/APIKit.git", from: "5.1.0")
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
     ],
     targets: [
         .target(
             name: "apple-device-manager",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "JWTKit", package: "jwt-kit"),
-                .product(name: "APIKit", package: "APIKit")
+                .product(name: "JWTKit", package: "jwt-kit")
             ]),
         .testTarget(
             name: "apple-device-managerTests",
